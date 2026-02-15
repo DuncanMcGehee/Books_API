d4075baf0b2cc821a2a412f50575eb240a12ea03
Duncan McGehee <duncanmcgehee26@gmail.com>
2026-02-15 18:49:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-// Books for bookstore API
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Sample movie data not shown
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Movie API server running at http://localhost:${port}`);
+});
+
+// Root endpoint - API homepage
+app.get('/', (req, res) => {
+    res.json({ 
+        message: "Welcome to the Movie API", 
+        endpoints: { 
+            "GET /movies": "Get all movies", 
+            "GET /movies/:id": "Get a specific movie by ID" 
+        } 
+    }); 
+});
+
+/*Books for bookstore API
 let books = [
     {
         id: 1,
@@ -23,6 +48,7 @@ let books = [
     }
     // Add more books if you'd like!
 ];
+*/
 
 /* Create your REST API here with the following endpoints:
     'GET /api/books': 'Get all books',
